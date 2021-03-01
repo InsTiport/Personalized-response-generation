dc5bed7c807be1e788ed0afd59b0ca14b6a9a0d6
InsTiport <hfsun98@gmail.com>
2021-03-01 14:26:31 -0500

    completing script that is able to generate a Pytorch dataset

---
+++ processing/generate_dataset.py
@@ -0,0 +1,19 @@
+from torchtext.data import TabularDataset, BucketIterator, RawField
+import os
+
+BATCH_SIZE = 10
+
+os.chdir('../')
+
+question = RawField()
+response = RawField()
+fields = {'question': ('q', question), 'response': ('r', response)}
+
+dataset = TabularDataset(path=os.path.join('data', 'csv', 'single_turn_utterance.csv'), format='csv', fields=fields)
+
+iterator = BucketIterator(dataset=dataset, batch_size=BATCH_SIZE)
+
+for batch in iterator:
+    # sample usage
+    print(batch.q)
+    print(batch.r)
