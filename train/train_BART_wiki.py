6ebe0a7fb5801030b932abc1c9d70c445e295345
Ziyuan Cao <zcao300@gatech.edu>
2021-05-02 10:17:35 +0800

    catch cuda error in bart wiki

--- train/train_BART_wiki.py
+++ train/train_BART_wiki.py
@@ -29,6 +29,7 @@ arg_parser.add_argument(
     default=5,
     help=f'Specify number of training epochs'
 )
+
 arg_parser.add_argument(
     '-b', '--batch',
     type=int,
@@ -67,7 +68,7 @@ if device == 'cuda':
     torch.cuda.set_device(DEVICE_ID)  # use an unoccupied GPU
 # load model
 model = Bart_wiki_model().to(device)
-sentence_encoder = SentenceTransformer('paraphrase-distilroberta-base-v1').to(device)
+sentence_encoder = SentenceTransformer('paraphrase-distilroberta-base-v1').to('cpu')
 # load tokenizer
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
 
@@ -79,6 +80,11 @@ optimizer_grouped_parameters = [
 ]
 optimizer = AdamW(optimizer_grouped_parameters, lr=1e-5)
 
+# load intermediate check point
+# checkpoint = torch.load(os.path.join('model_weights', f'{MODEL_NAME}_checkpoint.tar'))
+# model.load_state_dict(checkpoint['model_state_dict'])
+# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+# print("loaded")
 # record these for every epoch
 loss_record = []
 ppl_record = []
@@ -101,60 +107,71 @@ for epo in range(NUM_EPOCH):
     train_iterator_with_progress = tqdm.tqdm(data_loader)
     idx = 0
     for batch in train_iterator_with_progress:
-        batch_q = batch['question']
-        batch_r = batch['response']
-        batch_sw = batch['section_wiki']
-        batch_gw = batch['game_wiki']
+        
+        try:
 
-        section_wiki_encoding = torch.tensor(sentence_encoder.encode(batch_sw))
-        game_wiki_encoding = torch.tensor(sentence_encoder.encode(batch_gw))
+            batch_q = batch['question']
+            batch_r = batch['response']
+            batch_sw = batch['section_wiki']
+            batch_gw = batch['game_wiki']
 
-        # input encoding
-        input_encoding = tokenizer(batch_q, return_tensors='pt', padding=True, truncation=True)
-        input_ids = input_encoding['input_ids'].to(device)
-        input_attention_mask = input_encoding['attention_mask'].to(device)
+            section_wiki_encoding = torch.tensor(sentence_encoder.encode(batch_sw))
+            game_wiki_encoding = torch.tensor(sentence_encoder.encode(batch_gw))
 
-        # target encoding
-        # this kind of embedding will make the input to BART decoder be like </s> <s> content </s>
-        target_encoding = tokenizer(batch_r, return_tensors='pt', padding=True, truncation=True)
-        target_ids = target_encoding['input_ids'].to(device)
-        target_attention_mask = target_encoding['attention_mask'].to(device)
+            # input encoding
+            input_encoding = tokenizer(batch_q, return_tensors='pt', padding=True, truncation=True)
+            input_ids = input_encoding['input_ids'].to(device)
+            input_attention_mask = input_encoding['attention_mask'].to(device)
 
-        decoder_input_ids = target_ids.new_zeros(target_ids.shape)
-        decoder_input_ids[:, 1:] = target_ids[:, :-1].clone()
-        decoder_input_ids[:, 0] = model.bart.config.decoder_start_token_id
+            # target encoding
+            # this kind of embedding will make the input to BART decoder be like </s> <s> content </s>
+            target_encoding = tokenizer(batch_r, return_tensors='pt', padding=True, truncation=True)
+            target_ids = target_encoding['input_ids'].to(device)
+            target_attention_mask = target_encoding['attention_mask'].to(device)
 
-        decoder_attention_mask = target_attention_mask.new_ones(target_attention_mask.shape)
-        decoder_attention_mask[:, 1:] = target_attention_mask[:, :-1].clone()
+            decoder_input_ids = target_ids.new_zeros(target_ids.shape)
+            decoder_input_ids[:, 1:] = target_ids[:, :-1].clone()
+            decoder_input_ids[:, 0] = model.bart.config.decoder_start_token_id
 
-        labels = target_ids.clone()
-        labels[labels == model.bart.config.pad_token_id] = -100
+            decoder_attention_mask = target_attention_mask.new_ones(target_attention_mask.shape)
+            decoder_attention_mask[:, 1:] = target_attention_mask[:, :-1].clone()
 
-
-        # zero-out gradient
-        optimizer.zero_grad()
-
-        # forward pass
-        logits = model(input_ids=input_ids,
-                        attention_mask=input_attention_mask,
-                        decoder_input_ids=decoder_input_ids,
-                        decoder_attention_mask=decoder_attention_mask,
-                        section_wiki_encoding=section_wiki_encoding,
-                        game_wiki_encoding=game_wiki_encoding)
+            labels = target_ids.clone()
+            labels[labels == model.bart.config.pad_token_id] = -100
 
 
-        # compute loss and perform a step
-        logits = logits.reshape(-1, logits.shape[-1]).to(device)
-        labels = labels.reshape(-1).to(device)
-        loss = nn.CrossEntropyLoss(ignore_index=-100)(logits, labels)
-        loss.backward()
-        optimizer.step()
+            # zero-out gradient
+            optimizer.zero_grad()
 
-        idx += 1
+            # forward pass
+            logits = model(input_ids=input_ids,
+                            attention_mask=input_attention_mask,
+                            decoder_input_ids=decoder_input_ids,
+                            decoder_attention_mask=decoder_attention_mask,
+                            section_wiki_encoding=section_wiki_encoding,
+                            game_wiki_encoding=game_wiki_encoding)
 
-        total_loss += float(loss)
-        train_iterator_with_progress.set_description(f'Epoch {epo}')
-        train_iterator_with_progress.set_postfix({'Loss': loss.item()})
+
+            # compute loss and perform a step
+            logits = logits.reshape(-1, logits.shape[-1]).to(device)
+            labels = labels.reshape(-1).to(device)
+            loss = nn.CrossEntropyLoss(ignore_index=-100)(logits, labels)
+            loss.backward()
+            optimizer.step()
+
+            idx += 1
+
+            total_loss += float(loss)
+            train_iterator_with_progress.set_description(f'Epoch {epo}')
+            train_iterator_with_progress.set_postfix({'Loss': loss.item()})
+        except Exception as e:
+            print(e)
+            torch.save({'epoch': epo, 
+                        'model_state_dict': model.state_dict(), 
+                        'optimizer_state_dict': optimizer.state_dict()},
+                        os.path.join('model_weights', f'{MODEL_NAME}_checkpoint.tar'))
+            print("check point saved")
+            sys.exit()
 
     loss_record.append(total_loss)
     print(f'Loss in epoch {epo}: {total_loss}')
