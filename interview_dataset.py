8a5f642aebf267fadce1bf76d99cb992b57bc67a
InsTiport <hfsun98@gmail.com>
2021-06-21 20:05:24 -0400

    ?

--- interview_dataset.py
+++ interview_dataset.py
@@ -80,7 +80,7 @@ class InterviewDatasetESPN(torch.utils.data.Dataset):
                 try:
                     with open(news) as f:
                         all_news.append(f.read())
-                except FileNotFoundError or UnicodeEncodeError:
+                except:
                     file_path_rev = news[::-1]
                     folder_path = file_path_rev[file_path_rev.index('/'):][::-1]
 
