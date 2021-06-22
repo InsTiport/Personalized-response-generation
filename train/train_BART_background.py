833734f21da9896e21ca72d06ee9ac0278e4c2af
Ziyuan Cao <zcao300@gatech.edu>
2021-06-22 14:20:23 +0800

    update training scripts

--- train/train_BART_background.py
+++ train/train_BART_background.py
@@ -8,7 +8,7 @@ import os
 import tqdm
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-from interview_dataset import InterviewDataset
+from interview_dataset import InterviewDatasetESPN
 
 # setup args
 arg_parser = argparse.ArgumentParser()
@@ -86,7 +86,7 @@ for epo in range(NUM_EPOCH):
     '''
     DataLoader
     '''
-    dataset = InterviewDataset()
+    dataset = InterviewDatasetESPN()
     data_loader = torch.utils.data.DataLoader(
         dataset,
         batch_size=BATCH_SIZE,
@@ -143,7 +143,7 @@ for epo in range(NUM_EPOCH):
         '''
         DataLoader
         '''
-        valid_dataset = InterviewDataset(data='dev')
+        valid_dataset = InterviewDatasetESPN(data='dev')
         valid_data_loader = torch.utils.data.DataLoader(
             valid_dataset,
             batch_size=BATCH_SIZE,
